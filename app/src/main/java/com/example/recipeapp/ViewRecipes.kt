c470fa04f1b87dac7a229518ddb4044c68b8ca98
areejsmac <joo123816@gmail.com>
2021-10-11 10:23:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recipeapp/ViewRecipes.kt
@@ -0,0 +1,55 @@
+package com.example.recipeapp
+
+import android.app.ProgressDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ViewRecipes : AppCompatActivity() {
+    private var recipeDetails: List<RecipeDetails.Datum>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_recipes)
+
+        val responseText = findViewById<View>(R.id.textView2) as TextView
+
+        getRecipes(onResult = {
+            recipeDetails = it
+            Log.e("Data", recipeDetails.toString())
+
+            var stringToBePritined:String? = "";
+            for(recipe in recipeDetails!!){
+                stringToBePritined = stringToBePritined +recipe.title + "\n"+recipe.author + "\n"+recipe.ingredients + "\n"+recipe.instructions+ "\n\n"
+            }
+            responseText.text= stringToBePritined
+        } );
+    }
+    private fun getRecipes(onResult: (List<RecipeDetails.Datum>?) -> Unit) {
+        val apiInterface = APIClient().getClient()?.create(APIInterface::class.java)
+
+        if (apiInterface != null) {
+            apiInterface.getRecipies()?.enqueue(object : Callback<List<RecipeDetails.Datum>> {
+                override fun onResponse(
+                    call: Call<List<RecipeDetails.Datum>>,
+                    response: Response<List<RecipeDetails.Datum>>
+                ) {
+                    onResult(response.body())
+
+                }
+
+                override fun onFailure(call: Call<List<RecipeDetails.Datum>>, t: Throwable) {
+                    onResult(null)
+                    Toast.makeText(applicationContext, ""+t.message, Toast.LENGTH_SHORT).show();
+                }
+
+            })
+        }
+    }
+}
